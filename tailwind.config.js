d74ec1cd9a6eb4087b64c66ed3c60e89e537fd36
El-Behery <dev.elbehery@gmail.com>
2023-02-28 00:23:27 +0200

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //todo for if using `src` directory
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
