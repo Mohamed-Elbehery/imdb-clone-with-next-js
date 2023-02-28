669f12b6ae1b1083a5f8ccc902790e68b403a5c4
El-Behery <dev.elbehery@gmail.com>
2023-02-28 04:23:37 +0200

    fetch data from tmdb api and add it to the pages

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,11 @@
+const Movies = ({ movies }) => {
+  return (
+    <div>
+      {movies?.map((movie) => (
+        <p key={movie.name}>{movie.original_title}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Movies;
